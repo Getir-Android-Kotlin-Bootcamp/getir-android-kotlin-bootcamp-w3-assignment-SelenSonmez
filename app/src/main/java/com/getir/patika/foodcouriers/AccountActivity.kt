6c5c81a6af0942f7637c1ce85d4410c9a652cc69
selensonmez <selensonmez2001@gmail.com>
2024-04-05 16:02:04 +0300

    removed unnecessary comment

--- app/src/main/java/com/getir/patika/foodcouriers/AccountActivity.kt
+++ app/src/main/java/com/getir/patika/foodcouriers/AccountActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-//ben yaptım
 class AccountActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
